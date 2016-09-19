ff6ccaf9d113183b5cdbe0bebdcf27b718bd59ac
James Lebonitte <jlebonitte@gmail.com>
2016-09-18 21:05:02 -0400

    testing my ci/cd pipeline

--- src/client/app/MainContentView.jsx
+++ src/client/app/MainContentView.jsx
@@ -6,7 +6,7 @@ class MainContentView extends React.Component {
   render () {
     return (
         <div className="inner-main">
-             <StockStreamer />
+             test update jimbo!
         </div>
     );
   }
