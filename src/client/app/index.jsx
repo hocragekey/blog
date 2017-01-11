4e532cc51230b5c8dd59c8c087ff5404b016b5b4
Jim Lebonitte <jim.lebonitte@nielsen.com>
2017-01-10 22:15:42 -0500

    latest updates

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -3,22 +3,32 @@ import {render} from 'react-dom';
 
 require('../scss/master.scss');
 
-import MainNavigation from './MainNavigation.jsx';
-import MainContentView from './MainContentView.jsx';
-import BottomNavigation from './BottomNavigation.jsx';
-
 class App extends React.Component {
+        
   render () {
     return (
-        <div id="app">
-            <div id="blog-app-nav">
-                <MainNavigation />
+        <div className="hero is-fullheight is-primary">
+            <div className="hero-body">
+                <div className="container">
+                  <h1 className="title">
+                    Jim Lebonitte
+                  </h1>
+                  <h2 className="subtitle">
+                    Full Stack Software Architect Specializing in Innovative Technology Implementations
+                  </h2>
+                  <h2 className="subtitle">
+                    <i className="fa fa-docker" aria-hidden="true" />
+                  </h2>
+                </div> 
             </div>
-            <div className="main">
-                <MainContentView />
-            </div>
-            <div className="footer">
-                <BottomNavigation />
+            <div className="hero-foot">
+                <div className="container has-text-centered">
+                    <div className="hi-icon-wrap hi-icon-effect-3 hi-icon-effect-3a">
+                        <i className="hi-icon fa fa-arrow-down" aria-hidden="true">
+                            <a href="#"></a>
+                        </i>
+                    </div>
+                </div>
             </div>
         </div>
     );
